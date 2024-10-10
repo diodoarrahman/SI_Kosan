7286eb40f2780c0bae227c00a11a966a2d4d444e
deodo <deodoarrahman2@gmail.com>
2024-10-10 16:38:24 +0700

    nambahkan controller landing page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/landingpage', [App\Http\Controllers\LandingPageController::class,'index']);
